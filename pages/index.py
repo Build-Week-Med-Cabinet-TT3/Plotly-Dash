71131a8bd6b203df11038e8e400d531a1d19425e
EEdwardsA <ecomstockedwards@gmail.com>
2020-11-16 16:51:57 -0500

    Changed text and formatting on index landing page, uploaded potential background img to assets

--- pages/index.py
+++ pages/index.py
@@ -15,30 +15,42 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
-            ## Your Value Proposition
-
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
 
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/predictions')
     ],
     md=4,
 )
 
-gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=60)
-
 column2 = dbc.Col(
     [
-        dcc.Graph(figure=fig),
-    ]
+        dcc.Markdown(
+            """
+
+            """
+        ),
+    ],
+    md=4,
 )
 
-layout = dbc.Row([column1, column2])
+
+
+column3 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+        
+            ## Are you ready to try medical cannabis?
+
+            Med Cabinet listens to you and your needs. Do you need help sleeping? Pain relief? Do you want to feel calm, or energetic?
+
+            Med Cabinet is created for the first-time MMJ user, who wants to explore the therapeutic possibilities of cannabis.
+
+            """
+        ),
+        dcc.Link(dbc.Button('Click here to try it out', color='primary'), href='/predictions')
+    ],
+    md=4,
+)
+
+layout = dbc.Row([column1, column2, column3])
